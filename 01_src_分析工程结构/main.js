d62a5b87c49f29333774c3b1a09ee53c0780db41
fengjiahao66666 <2934182834@qq.com>
2023-02-18 11:10:43 +0800

    setup

--- 01_src_分析工程结构/main.js
+++ 01_src_分析工程结构/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 //创建应用实例对象--app（类似于之前Vue2中的vm，但app比vm更“轻”）
 // createApp(App).mount('#app')
 const app = createApp(App)
+console.log('@@@',app);
 
 //挂载
 app.mount('#app')
