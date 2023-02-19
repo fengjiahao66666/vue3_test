a1d2dc34104329b8a69ae3e7d62dd48e679d822e
fengjiahao66666 <2934182834@qq.com>
2023-02-19 20:29:06 +0800

    provide,inject

---
+++ 18_src_provide与inject/components/Child.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="child">
+		<h3>我是Child组件（子）{{x.name}}--{{x.price}}</h3>
+		<Son/>
+	</div>
+</template>
+
+<script>
+/*eslint-disable*/
+	import {inject} from 'vue'
+	import Son from './Son.vue'
+	export default {
+		name:'Child',
+		components:{Son},
+		setup(){
+			let x = inject('car')
+			console.log(x,'Child-----')
+			return {x}
+		} 
+	}
+</script>
+
+<style>
+	.child{
+		background-color: skyblue;
+		padding: 10px;
+	}
+</style>
